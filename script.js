d1ef5c721942181ce7dc6927582fb88e0f5ded04
fady-albert <fadyalber2009@gmail.com>
2026-07-24 00:09:53 +0300

    finish mode button

--- script.js
+++ script.js
@@ -3,15 +3,27 @@ const modeBtn = document.getElementById('mode');
 const body = document.querySelector('body');
 const modeBtnTxt = document.getElementById('modeIcon');
 
+// js data
+const savedMode = localStorage.getItem('mode') || '';
+
 // mode function
 function mode() {
     body.classList.toggle('dark');
+
     setTimeout(() => {
         modeBtnTxt.textContent = modeBtnTxt.textContent === 'light_mode' ? 'dark_mode' :'light_mode';
     }, 500);
 }
 
+// change mode by click on the button
 modeBtn.addEventListener('click', () => {
     mode()
     modeBtn.classList.toggle('rotate');
+    // save the mode that the user use it
+    localStorage.setItem('mode', savedMode === 'light' ? 'dark' : 'light');
 })
+
+// check if user used to use dark mode
+if(savedMode === 'dark') {
+    mode()
+}
