f234bf3590e479380fc56d77a5843bb319996b84
fady-albert <fadyalber2009@gmail.com>
2026-07-24 00:54:46 +0300

    add main function

--- script.js
+++ script.js
@@ -2,9 +2,14 @@
 const modeBtn = document.getElementById('mode');
 const body = document.querySelector('body');
 const modeBtnTxt = document.getElementById('modeIcon');
+const mainCon = document.getElementById('con');
 
 // js data
 const savedMode = localStorage.getItem('mode') || '';
+let squareH = 2;
+let squareW = 2;
+let squareDataH = 2;
+let squareDataW = 2;
 
 // mode function
 function mode() {
@@ -27,3 +32,29 @@ modeBtn.addEventListener('click', () => {
 if(savedMode === 'dark') {
     mode()
 }
+
+// the main function
+function createSquare() {
+    for(let i = 0; i < (squareH + squareW); i++) {
+        const div = document.createElement('div');
+        div.classList.add('square');
+        mainCon.appendChild(div);
+        mainCon.style.gridTemplateColumns = `repeat(${squareW}, auto)`
+
+        createNumPlace(div)
+    }
+}
+
+function createNumPlace(place) {
+    for(let i = 0; i < (squareDataH + squareDataW); i++) {
+        const div = document.createElement('div');
+        div.classList.add('num');
+        div.style.width = '25px';
+        div.style.height = '25px';
+        div.style.background = '#000'
+        place.appendChild(div)
+        place.style.gridTemplateColumns = `repeat(${squareW}, auto)`
+    }
+}
+
+createSquare()
